c6bcac65a32091f94dec30ecea403d20170356ff
Henrynaut <neilmchenry@gmail.com>
2018-05-08 00:43:43 -0500

    Added gitignore and updated splines
    gitignore to ignore .asv files and added more configurability to the spline dynamics code by linking the sixdof and bodyDynamics Inputs

---
+++ bodyDynamics.m
@@ -0,0 +1,63 @@
+
+function [omegax,omegay,omegaz,pos] = bodyDynamics(tarray,fm,dim,ihwd)
+% Written by Neil McHenry
+% AERO 650
+
+%Tarray input is a matrix of time intervals, e.g. [1:10]
+%fm input is a 3x2 matrix of the forces and moments e.g.:
+    % Forces = [.2; 0; .3];
+    % Moments = [20; .4; -2];
+    % fm = [Forces,Moments];
+
+% dim and Inrt are inputs for Ihwd for a solid cuboid of height h, 
+%width w, depth d, mass m, (1x4 and 1x3) e.g.:
+
+    % h = 0.8;  %0.5m
+    % w = 0.5;  %0.5m
+    % d = 0.5;  %0.5m
+    % m = 8.2;  %8.2kg
+    % dim = ([h,w,d,m]);
+
+    % Ih = 1/12*m*(w^2+d^2);
+    % Iw = 1/12*m*(d^2+h^2);
+    % Id = 1/12*m*(w^2+h^2);
+    % ihwd = ([Ih,Iw,Id]);
+
+    %Assign Values based on Inputs
+    Forces = fm(:,1);
+    Moments = fm(:,2);
+    Mass = dim(4);
+    
+    Ih = ihwd(1);
+    Iw = ihwd(2);
+    Id = ihwd(3);
+    Inertia = [Ih,0,0;
+              0, Iw ,0;
+              0,0, Id];
+%tarray = [1:10];
+
+[t,y] = sixdof(Forces, Moments, Mass, Inertia, tarray);
+
+%% Calculating Star Plot Projection due to Dynamics
+
+%Part A: Convert euler rates & body position from Body frame to Inertial frame
+
+%Part B: Calculate Projections based on Rotations and Displacement
+    % Component 1: Dislocation in X due to X rotation
+
+    % Component 2: Dislocation in Y due to Y rotation
+    
+    % Component 3: Dislocation in X & Y due to Z rotation
+    
+    % Component 4: Sum dislocations
+    
+ %Part C: Output Xp and Yp over time (projection based on orientation/position)
+
+ omegax = y(:,1);
+ omegay = y(:,2);
+ omegaz = y(:,3);
+ pos = y(:,7:9);
+
+  
+ 
+end
--- spline.m
+++ spline.m
@@ -1,10 +1,34 @@
-npts = 13;
-t = linspace(0,8*pi,npts);
-z = linspace(-1,1,npts);
-omz = sqrt(1-z.^2);
-xyz = [cos(t).*omz; sin(t).*omz; z];
-plot3(xyz(1,:),xyz(2,:),xyz(3,:),'ro','LineWidth',2);
-text(xyz(1,:),xyz(2,:),xyz(3,:),[repmat('  ',npts,1), num2str((1:npts)')])
+clc;
+close all;
+
+%Initialization
+%fm input is a 3x2 matrix of the forces and moments e.g.:
+    Forces = [.2; 0; .3];
+    Moments = [20; .4; -2];
+    fm = [Forces,Moments];
+
+% dim and Inrt are inputs for Ihwd for a solid cuboid of height h, 
+%width w, depth d, mass m, (1x4 and 1x3) e.g.:
+
+    h = 0.8;  %0.5m
+    w = 0.5;  %0.5m
+    d = 0.5;  %0.5m
+    m = 8.2;  %8.2kg
+    dim = ([h,w,d,m]);
+
+    Ih = 1/12*m*(w^2+d^2);
+    Iw = 1/12*m*(d^2+h^2);
+    Id = 1/12*m*(w^2+h^2);
+    ihwd = ([Ih,Iw,Id]);
+    
+    %Number of Steps
+    npts = 10;
+[omega1,omega2,omega3,pos] = bodyDynamics(1:npts,fm,dim,ihwd);
+pos = pos';
+plot3(pos(1,:),pos(2,:),pos(3,:),'ro','LineWidth',2);
+
+text(pos(1,:),pos(2,:),pos(3,:),[repmat('  ',npts,1), num2str((1:npts)')])
+
 ax = gca;
 ax.XTick = [];
 ax.YTick = [];
@@ -12,5 +36,7 @@ ax.ZTick = [];
 box on
 
 hold on
-fnplt(cscvn(xyz(:,[1:end 1])),'r',2)
+% fnplt(cscvn(xyz(:,[1:end 1])),'r',2)
+fnplt(cscvn(pos),'r',2)
+
 hold off
